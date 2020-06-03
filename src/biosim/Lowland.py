0c69e64ca9240adc2b1cc904befe777654e8761e
Petter Kolstad Hetland <Petter.hetland@gmail.com>
2020-06-03 21:46:24 +0200

    Updated Lowland tests and class

--- src/biosim/Lowland.py
+++ src/biosim/Lowland.py
@@ -4,7 +4,26 @@
 Lowland class for the simulation.
 """
 
+import numpy as np
+
 
 class Lowland:
+    """
+    Args...
+    """
     def __init__(self, f_max=800.0):
-        self.f_max = f_max
+        self.f_max = f_max  # Set fodder amount for each new year
+        self.fodder = f_max  # Set starting fodder amount to f_max
+        self.population = []  # List containing information about all animals in cell
+        self.population_count = len(self.population)  # Total population in cell
+
+
+    def add_animal(self, animal_class, attribute_values):
+        animal = animal_class
+        values = attribute_values
+        animal_dict = dict(animal, values)
+        self.population.append(animal_dict)  # Append new animal to population dict
+
+    @property
+    def herb_count(self):
+        return [np.count]
--- tests/test_lowland.py
+++ tests/test_lowland.py
@@ -15,3 +15,24 @@ def test_lowland_instance():
     lowland_default = Lowland()
     lowland_100 = Lowland(f_max=100.0)
 
+
+def test_fodder():
+    """Fodder attribute of instance can be accessed and has the right value"""
+    lowland = Lowland(f_max=200)
+    f_max = lowland.f_max  # Get f_max attribute value
+    lowland_fodder = lowland.fodder  # Get fodder attribute value
+    lowland_fodder = f_max  # Check that initial fodder amount is same a f_max attribute
+
+
+def test_population():
+    """Default values for population counts in cell are correct"""
+    lowland = Lowland()
+    assert lowland.population == 0
+    assert lowland.carn_count == 0
+    assert lowland.herb_count == 0
+
+
+def test_add_animal():
+    """Add a mockup animal to the cell"""
+    lowland = Lowland()
+    lowland.add_animal('herbivore', {'attr_1': 50.0, 'attr_2': 100.0})
