4046f09ab379271341695f830d64c28bac3bf4a1
Anders Høst <andersm_host@hotmail.com>
2020-06-04 20:33:34 +0200

    update, draft

--- src/biosim/animal.py
+++ src/biosim/animal.py
@@ -5,6 +5,7 @@ __email__ = 'anders.molmen.host@nmbu.no, petter.storesund.hetland@nmbu.no'
 
 
 class Animal:
+
     """
     Super class for Herbivores and Carnivores
     """
@@ -30,17 +31,32 @@ class Animal:
 
     """
     Source: Afternoon lecture INF200. 2. June 2020
-    Comment: Consider merging the two function in one
+    Comment: Consider merging the two function in one, not working as it should
+    Suggestions?
     """
-
-    def q(self, sgn, x, xhalf, phi):
+    @classmethod
+    def q(cls, sgn, x, xhalf, phi):
         return 1. / (1. + np.exp(sgn * phi * (x - xhalf)))
 
     """
     Source: Afternoon lecture INF200. 2. June 2020
     """
-
-    def fitness(self, age, weight, p):
+    @classmethod
+    def fitness(cls, q, age, weight):
+        p = {'w_birth': 8.0,
+             'sigma_birth': 1.5,
+             'beta': 0.9,
+             'eta': 0.05,
+             'a_half': 40,
+             'phi_age': 0.6,
+             'w_half': 10.0,
+             'phi_weight': 0.1,
+             'mu': 0.25,
+             'gamma': 0.2,
+             'zeta': 3.5,
+             'xi': 1.2,
+             'omega': 0.4,
+             'F': 10.0}
         return (q(+1, age, p['a_half'], p['phi_age'])
                 * q(-1, weight, p['w_half'], p['phi_weight']))
 
