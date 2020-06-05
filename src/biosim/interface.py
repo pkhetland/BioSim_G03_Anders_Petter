24c0e5e25d29111f28e30a0a35007c42ce10e78f
Petter Kolstad Hetland <Petter.hetland@gmail.com>
2020-06-05 17:13:36 +0200

    Updated interface.py

--- src/biosim/interface.py
+++ src/biosim/interface.py
@@ -8,11 +8,17 @@ with one animal in one cell.
 from src.biosim.animal import Animal, Herbivore
 from src.biosim.landscape import Lowland
 
+import textwrap
+
 
 class Simulation:
     def __init__(self):
-        self.landscape = Lowland()  # Create a hardcoded cell
-        self.animals = []
+        self.geogr = None
+        self.animals = [{'species': 'Herbivore',
+                         'age': 5,
+                         'weight': 20}
+                        for _ in range(150)]
+
         self.year = 0
 
     # def add_landscape(self, landscape_str):
@@ -23,25 +29,38 @@ class Simulation:
     #             pass
 
     def add_animals(self, animals):
-            self.animals = animals
+        for animal in animals:
+            self.animals.append(animal)
 
-    def run_year_cycle(self):
+    # def run_year_cycle(self):
+    #     #  1. Feeding
+    #     for animal in self.animals:
+    #         #  Sort animals by fitness
+    #
+    #         #
+    #         animal.feed(self.landscape.fodder)
+    #         self.landscape.fodder
 
-        self.year += 1
+        #  2. Procreation
+        #  3. Migration
+        #  4. Aging
+        #  5. Loss of weight
+        #  6. Death
 
-    def run_simulation(self, num_years):
-        for year in num_years:
-            if year % 10 == 0:  # Print every tenth year for progress
-                print(f'Simulation has been run for {year} years.')
-            self.run_year_cycle()
+        # self.year += 1
+
+    # def run_simulation(self, num_years):
+    #     for year in num_years:
+    #         if year % 10 == 0:  # Print every tenth year for progress
+    #             print(f'Simulation has been run for {year} years.')
+    #         self.run_year_cycle()
 
 
 if __name__ == '__main__':
+
     sim = Simulation()  # Create simple simulation instance
 
-    sim.add_animals([Herbivore(age=0, weight=10)])  # Add a single herbivore
-    print(sim.landscape, sim.animals)  # Print updated values
+    sim.animals
 
-    print(sim.landscape.fodder)
 
     # sim.run_simulation(num_years=200)
