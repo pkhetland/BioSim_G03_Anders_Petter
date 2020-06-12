8664db3c0d2a94451cbcede4e98486f526859409
Anders Høst <andersm_host@hotmail.com>
2020-06-12 10:14:07 +0200

    update animal. Remove code not used.

--- src/animal.py
+++ src/animal.py
@@ -13,7 +13,7 @@ class Animal:
     p = {}
 
     """
-    Super class for Herbivores and Carnivores. TEST
+    Super class for Herbivores and Carnivores.
     """
     instance_count = 0
 
@@ -219,26 +219,6 @@ class Herbivore(Animal):
                 "F": 10.0}
 
     def __init__(self, weight=10, age=0):
-        if self.p is None:  # If no parameters are specified
-            super().p.update({  # Insert default values for species
-                "w_birth": 8.0,
-                "sigma_birth": 1.5,
-                "beta": 0.9,
-                "eta": 0.05,
-                "a_half": 40.0,
-                "phi_age": 0.6,
-                "w_half": 10.0,
-                "phi_weight": 0.1,
-                "mu": 0.25,
-                "gamma": 0.2,
-                "zeta": 3.5,
-                "xi": 1.2,
-                "omega": 0.4,
-                "F": 10.0,
-            })
-        else:
-            super().get_params()
-
         super().__init__(weight, age)
 
         Herbivore.instance_count += 1
@@ -284,27 +264,6 @@ class Carnivore(Animal):
          "DeltaPhiMax": 10.0}
 
     def __init__(self, weight=None, age=0):
-        if self.p is None:  # If no parameters are specified
-            super().p.update({  # Insert default values for species
-                "w_birth": 6.0,
-                "sigma_birth": 1.0,
-                "beta": 0.75,
-                "eta": 0.125,
-                "a_half": 40.0,
-                "phi_age": 0.3,
-                "w_half": 4.0,
-                "phi_weight": 0.4,
-                "mu": 0.4,
-                "gamma": 0.8,
-                "zeta": 3.5,
-                "xi": 1.1,
-                "omega": 0.8,
-                "F": 50.0,
-                "DeltaPhiMax": 10.0,
-            })
-        else:
-            self.p = super().get_params()
-
         super().__init__(weight, age)
 
         Carnivore.instance_count += 1
