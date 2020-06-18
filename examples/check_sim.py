29a1af37453efa32d7304fa07aad57ae55594dd2
Petter Hetland <Petter.hetland@gmail.com>
2020-06-18 10:22:57 +0200

    Update make_movie.

--- examples/check_sim.py
+++ examples/check_sim.py
@@ -51,12 +51,12 @@ if __name__ == '__main__':
     sim = BioSim(island_map=geogr,
                  ini_pop=ini_herbs,
                  seed=123456,
+                 img_base='test',
                  hist_specs={'fitness': {
                      'max': 1.0, 'delta': 0.05},
                      'age': {'max': 60.0, 'delta': 2},
                      'weight': {'max': 60, 'delta': 2}
-                 },
-                 plot_graph=False)
+                 })
     # sim.set_animal_parameters('Herbivore', {'zeta': 3.2, 'xi': 1.8})
     # sim.set_animal_parameters('Carnivore', {'a_half': 70, 'phi_age': 0.5,
     #                                         'omega': 0.3, 'F': 65,
@@ -64,11 +64,13 @@ if __name__ == '__main__':
     #
     # sim.set_landscape_parameters('L', {'f_max': 700})
 
-    sim.simulate(num_years=100, vis_years=1, img_years=2000)
+    sim.simulate(num_years=200, vis_years=1, img_years=1)
 
-    sim.add_population(population=ini_carns)
+    sim.make_movie()
 
-    sim.simulate(num_years=2000, vis_years=1, img_years=2000)
+    # sim.add_population(population=ini_carns)
+
+    # sim.simulate(num_years=2000, vis_years=1, img_years=2000)
 
     # plt.savefig('check_sim.pdf')
 
